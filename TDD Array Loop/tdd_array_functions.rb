f92f001bd321543860ac3c892b8b811be1133d24
Dominic <mrdjfraser@gmail.com>
2017-03-08 17:19:01 +0000

    1-4 of tdd homework  was already done in first homework

--- TDD Array Loop/tdd_array_functions.rb
+++ TDD Array Loop/tdd_array_functions.rb
@@ -4,3 +4,23 @@ def add_array_lengths(array1, array2)
 end
 
 #2.
+def test_sum_array
+  result = sum_array( [ 1,2,3,4,5 ] )
+  assert_equal( 15, result )
+end
+
+#3.
+  if array.index(item) != nil 
+    return true
+  else
+    return false
+  end
+end
+
+#4.
+def get_first_key(teachers_wallets)
+    array_of_teachers = teachers_wallets.keys()
+    return array_of_teachers[0]
+end
+
+#5.
